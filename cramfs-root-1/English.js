de1ab8088923ff9fc82218f417daac96858209ca
digoo-rev <68204897+digoo-rev@users.noreply.github.com>
2020-07-13 00:17:24 +0200

    Add files via upload

---
+++ cramfs-root-1/English.js
@@ -0,0 +1,71 @@
+﻿var Translate={
+	title:"Digital Video Recorder",
+	usr:"Username:",
+	pswd:"Password:",
+	logi:"please login in!",
+	login:"Login",
+	dev:"device",
+	ch:"CAM ",
+	MFt:"MainStream",
+	SFt:"SubStream",
+	Zoom:"zoom",
+	Iris:"Iris",
+	Focu:"focus",
+	Split1:"View 1",
+	Split4:"View 4",
+	Split9:"View 9",
+	Split16:"View 16",
+	Split25:"View 25",
+	Split36:"View 36",
+	PlayAll:"connect all video",
+	StopPlayAll:"close all video",
+	mainPlayAll:"playAll(mainStream)",
+	subPlayAll:"playAll(subStream)",
+	ok:"ok",
+	Cancel:"Cancel",
+	logout:"Logout",
+	autoPrompt:"Auto Prompt",
+	version:"Version"
+}
+
+var Versions=
+{   
+    vidioIn:"Video In: ",
+    audioIn:"Audio In: ",
+    alarmIn:"Alarm In: ",
+    alarmOut:"Alarm Output: ",
+    buildTime:"Build Date:",
+    version:"System: ",
+    serialNo:"Serial ID: " ,
+    ok:"ok" 
+    
+}
+
+var Password=
+{
+	findpsw:"Retrieve Password",
+	verifytype:"verifytype",
+	op0:"select",
+	op1:"verify the security question",
+	op2:"verify the email ",
+	question1:"Question1: ",
+	answer:"Answer: ",
+	question2:"Question2: ",
+	email:"email：",
+	yzmbtn:"Get validation code",
+	emailcode:"Verification Code：",
+	enteryzm:"Please enter the verification code",
+	newpassword:"NewPassword: ",
+	confirmpassword:"Confirm: ",
+	enteranswer:"Please enter the answer",
+	enterpassword:"Please enter a password(Password only Numbers and letters)",
+	confirmpsw:"Please confirm the password(Password only Numbers and letters)",
+	ok:"Submit",
+	back:"Back"	,
+	NewPswLogin:"Click the Back button to USE the New password",
+	RetryRevision:"Click the Back button, try again",
+	notsupportemail:"E - mail support to recover non - secure password",
+	notsetemail:"Please before setting the mail password",
+	notsupportques:"Support for recovering password WITHOUT security problem",
+	notsetques:"Please first, for safety reasons"
+}
