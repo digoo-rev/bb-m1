33bad1d0bba21983772c228994c31a578be26c59
digoo-rev <68204897+digoo-rev@users.noreply.github.com>
2020-07-13 00:13:02 +0200

    Add files via upload

---
+++ cramfs-root-0/bin/app.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+/usr/bin/Sofia
+echo 1 > /mnt/mtd/Config/RebootAbnormal
+cp /var/sofia.log  /mnt/mtd/sofia.log
+dmesg > /var/halted.log
+date >> /var/halted.log
+cat /proc/umap/* >> /var/halted.log
+cat /proc/meminfo >> /var/halted.log
+tail -c 20480 /var/halted.log > /mnt/mtd/halted.log
